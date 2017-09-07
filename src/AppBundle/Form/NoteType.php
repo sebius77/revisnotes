eb6d023698b30abe193bf7acaa0de1d6e31e2f33
Gaudin Sébastien <sebastien.gaudin@u-psud.fr>
2017-09-07 10:08:19 +0200

    ajout du répertoire Form

---
+++ src/AppBundle/Form/NoteType.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace AppBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class NoteType extends AbstractType
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('titre',                      TextType::class)
+            ->add('enonce',                     TextareaType::class)
+            ->add('solution',                   TextareaType::class)
+
+
+            ->add('Ajouter',                    SubmitType::class)
+            ;
+    }
+    
+    /**
+     * {@inheritdoc}
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'AppBundle\Entity\Note'
+        ));
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getBlockPrefix()
+    {
+        return 'appbundle_note';
+    }
+
+
+}
