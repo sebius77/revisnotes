4ebcb1cfe6e16eb684780f3c00df8f537bcdd9fc
Sébastien Gaudin <gebs.dev@gmail.com>
2017-10-21 21:17:30 +0200

    ajout page d'erreur

--- src/AppBundle/Controller/AdminController.php
+++ src/AppBundle/Controller/AdminController.php
@@ -5,12 +5,13 @@ namespace AppBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
-
+use Symfony\Component\HttpFoundation\Request;
 
 class AdminController extends Controller
 {
     /**
      * @Route("/admin/users",name="adminUsers")
+     * Permet l'affichage de tous les comptes
      */
     public function adminAction()
     {
@@ -24,6 +25,49 @@ class AdminController extends Controller
         return $this->render('AppBundle:Admin:admin.html.twig', array(
             'users' => $users,
         ));
+
+    }
+
+
+    /**
+     * @Route("blockUser/{id}", name="blockUser", requirements={"id" = "\d+"})
+     */
+    public function blockAction(Request $request, $id)
+    {
+        // On récupère l'entity Manager
+        $em = $this->getDoctrine()->getManager();
+
+        // On effectue une requête pour récupérer le User
+        $user = $em->getRepository('GebsUserBundle/Repository/User')
+            ->find($id);
+
+
+        if(!null === $user)
+        {
+            $user->setEnabled(false);
+            $em->persist($user);
+
+            $em->flush();
+
+            $this->addFlash(
+                'notice',
+                'L\'utilisateur ' . $user->getUsername() . ' a été bloqué! '
+            );
+
+            return $this->redirectToRoute('adminUsers');
+
+        } else
+        {
+            $this->addFlash(
+                'notice',
+                'L\'utilisateur n\'existe pas! '
+            );
+
+            return $this->redirectToRoute('adminUsers');
+        }
+
+
+
 
     }
 
