151687f08da07315e2dfcdfdb1088bf44bae1201
Sébastien Gaudin <gebs.dev@gmail.com>
2017-11-22 13:31:26 +0100

    correction probleme javascript pour la suppression d'un utilisateur

--- web/js/delUser.js
+++ web/js/delUser.js
@@ -1,16 +1,15 @@
 // web/js/delUser.js
 
+console.log('test au chargement du fichier js');
+
 // Dans le cas ou l'on clique sur le bouton d'ajout en Bdd, on exécute une requête Ajax.
-$('#delButtonUser').click(function() {
+$(':button').click(function() {
 
-
-    //var deleteUser = "http://localhost/revisnotes/web/app_dev.php/admin/deleteUser/" + $(this).val();
     var deleteUser = Routing.generate('deleteUser', { id: $(this).val() });
 
+    console.log(deleteUser);
 
-    $.get(deleteUser , function(reponse){
-
-        console.log(reponse.message);
+    $.get(deleteUser, function(reponse){
 
         if(reponse.message === true) {
             $('#divMessage').append('<div class="alert alert-success">L\'utilisateur a bien été supprimée !!!</div>');
